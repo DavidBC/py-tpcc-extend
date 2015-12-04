41cac4a861f7f8a984291449ebb84bdabbbf91f2
Heetae Kim <jason.heetae.kim@gmail.com>
2015-12-04 09:33:01 -0800

    Added benchmark results

---
+++ results/csv-splitter.sh
@@ -0,0 +1,30 @@
+cat outputResults_around.csv | grep DELIVERY > workload/outputResults_around_DELIVERY.csv
+cat outputResults_nofc.csv | grep DELIVERY > workload/outputResults_nofc_DELIVERY.csv
+cat outputResults_back.csv | grep DELIVERY > workload/outputResults_back_DELIVERY.csv
+cat outputResults_back_wo.csv | grep DELIVERY > workload/outputResults_back_wo_DELIVERY.csv
+cat outputResults_thru.csv | grep DELIVERY > workload/outputResults_thru_DELIVERY.csv
+
+cat outputResults_around.csv | grep NEW_ORDER > workload/outputResults_around_NEW_ORDER.csv
+cat outputResults_nofc.csv | grep NEW_ORDER > workload/outputResults_nofc_NEW_ORDER.csv
+cat outputResults_back.csv | grep NEW_ORDER > workload/outputResults_back_NEW_ORDER.csv
+cat outputResults_back_wo.csv | grep NEW_ORDER > workload/outputResults_back_wo_NEW_ORDER.csv
+cat outputResults_thru.csv | grep NEW_ORDER > workload/outputResults_thru_NEW_ORDER.csv
+
+cat outputResults_around.csv | grep ORDER_STATUS > workload/outputResults_around_ORDER_STATUS.csv
+cat outputResults_nofc.csv | grep ORDER_STATUS > workload/outputResults_nofc_ORDER_STATUS.csv
+cat outputResults_back.csv | grep ORDER_STATUS > workload/outputResults_back_ORDER_STATUS.csv
+cat outputResults_back_wo.csv | grep ORDER_STATUS > workload/outputResults_back_wo_ORDER_STATUS.csv
+cat outputResults_thru.csv | grep ORDER_STATUS > workload/outputResults_thru_ORDER_STATUS.csv
+
+cat outputResults_around.csv | grep PAYMENT > workload/outputResults_around_PAYMENT.csv
+cat outputResults_nofc.csv | grep PAYMENT > workload/outputResults_nofc_PAYMENT.csv
+cat outputResults_back.csv | grep PAYMENT > workload/outputResults_back_PAYMENT.csv
+cat outputResults_back_wo.csv | grep PAYMENT > workload/outputResults_back_wo_PAYMENT.csv
+cat outputResults_thru.csv | grep PAYMENT > workload/outputResults_thru_PAYMENT.csv
+
+cat outputResults_around.csv | grep STOCK_LEVEL > workload/outputResults_around_STOCK_LEVEL.csv
+cat outputResults_nofc.csv | grep STOCK_LEVEL > workload/outputResults_nofc_STOCK_LEVEL.csv
+cat outputResults_back.csv | grep STOCK_LEVEL > workload/outputResults_back_STOCK_LEVEL.csv
+cat outputResults_back_wo.csv | grep STOCK_LEVEL > workload/outputResults_back_wo_STOCK_LEVEL.csv
+cat outputResults_thru.csv | grep STOCK_LEVEL > workload/outputResults_thru_STOCK_LEVEL.csv
+
